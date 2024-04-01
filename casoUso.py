839fa0fed41c6b8f1978ed22a984c73fbf7eaaa9
Lourdes-Cruz <102762660+Lourdes-Cruz@users.noreply.github.com>
2024-03-31 20:01:22 -0600

    Add files via upload

---
+++ casoUso.py
@@ -0,0 +1,23 @@
+# Se importan los archivos necesarios
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+
+# Se leen los archivos parquet desde los vínculos
+transacciones_df = pd.read_parquet('https://datamxdevsa.blob.core.windows.net/crskmex000/DSTest/0saldos.parquet')
+saldos_df = pd.read_parquet('https://datamxdevsa.blob.core.windows.net/crskmex000/DSTest/0transferencias.parquet')
+clientes_df = pd.read_parquet('https://datamxdevsa.blob.core.windows.net/crskmex000/DSTest/0clientes.parquet')
+
+encabezados = transacciones_df.columns
+print(encabezados)
+
+encabezados1 = saldos_df.columns
+print(encabezados1)
+
+encabezados2 = clientes_df.columns
+print(encabezados2)
+
+# Se unen las tablas en un solo DataFrame
+merged_df = pd.merge(transacciones_df, saldos_df, on=['Contrato'])   #, 'CLIENTE', 'PRODUCTO'])
+merged_df = pd.merge(merged_df, clientes_df, on='NroDocum')
